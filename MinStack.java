b3e2f1483269a769914bdaff42722be234fa6c78
Anitha-K2006 <anitha3112kumar@gmail.com>
2025-12-23 14:52:14 +0700

    Create MinStack.java

---
+++ MinStack.java
@@ -0,0 +1,31 @@
+import java.util.*;
+class MinStack {
+    Stack<Integer>stack;
+    Stack<Integer>MinStack;
+    public MinStack() {
+        stack=new Stack<>();
+        MinStack=new Stack<>();
+    }
+    public void push(int val) {
+        stack.push(val);
+        if(MinStack.isEmpty()||val<=MinStack.peek()){
+            MinStack.push(val);
+        }
+    }
+    public void pop() {
+        int c=stack.pop();
+        if(c==MinStack.peek()){
+            MinStack.pop();
+        }
+
+    }
+    public int top() {
+        int res=stack.peek();
+        return res;
+    }
+    
+    public int getMin() {
+         int r=MinStack.peek();
+         return r;
+    }
+}
